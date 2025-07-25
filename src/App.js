01afc32415ba166bafced4b2e4b6f57ccbed0e27
Wellington Rodrigues <wellingtonrodrigues.rj@gmail.com>
2025-07-24 23:01:16 -0300

    Criação da estrtura básica do projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./Pages/Home";
+import "./App.css";
+
+export default function App()
+{
+    // Tela de inicilização
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+
+
+export default function Home()
+{
+    // Aqui vai ter a lista de org e os demais botões para criar/editar entre outros.
+
+    return(
+        <>
+            Home
+        </>
+    );
+}
